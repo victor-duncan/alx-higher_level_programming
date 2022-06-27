7505e84b4effe9578f390b54ab92b03b16324c64
Ahortu Victor Duncan <101404740+victor-duncan@users.noreply.github.com>
2022-06-27 20:29:49 +0000

    Hello World

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,26 +1,30 @@
-#include "list.h"
-
+#include "lists.h"
 /**
-* check_cycle - checks is a linked list contains a cycle
-* @list: linked list to check
-*
-*Return: 1 if the list has a cycle, 0 if it does not 
-*/
+ * check_cycle -linked list cycle
+ * @list: the pointer to the struct
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
 int check_cycle(listint_t *list)
 {
-    listint_t *slow = list;
-    listint_t *fast = list;
+	listint_t *a = list;
+	listint_t *b = list;
 
-    if (!list)
-            return (0);
-    
-    while (slow && fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-        if (slow == fast)
-                return (1);
-    }
+	if (list == NULL)
+	{
+		return (0);
+	}
 
-    return (0);
+	b = b->next;
+
+	while (a != NULL && b != NULL && b->next != NULL)
+	{
+		if (b == a)
+		{
+			return (1);
+		}
+		b = b->next->next;
+		a = a->next;
+	}
+	return (0);
 }
